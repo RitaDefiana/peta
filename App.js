b6aa3cc02bb698d501348e80bad9b0213b650a7a
RitaDefiana <37035695+RitaDefiana@users.noreply.github.com>
2018-05-27 22:11:41 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
+
+  state = {
+    region: {
+      latitude: -8.149407,
+      longitude: 115.216667,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.1162515,
+          longitude: 115.0892459
+        },
+        title: 'Bank BRI Singaraja',
+        subtitle: ''
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.1264492,
+          longitude: 115.0801901
+        },
+        title: 'Western Union',
+        subtitle: ''
+      },
+      {
+        key:3,
+        latlng: {
+          latitude:-8.1213245,
+          longitude: 115.0715694
+        },
+        title: 'Bank Mega Syariah'
+      }
+      ,
+      {
+        key:4,
+        latlng: {
+          latitude:-8.1113161,
+          longitude: 115.0860776
+        },
+        title: 'Bank Mandiri Singaraja'
+      }
+   ]
+  };
+
+  render() {
+    return (
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              Letak Bank di Singaraja
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}> http://rita.defiana@gmail.com </Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+});
